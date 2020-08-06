0f706ca9b82b8c033922bb45f785f46d0c1251b7
bunrarit khumman <newnewza147258@gmail.com>
2020-08-06 16:13:24 +0700

    ทดสอบvuetifli

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div> <v-btn color="primary">Home</v-btn>
+   <v-btn color="error">Home</v-btn>
+   <v-btn class="mx-2" fab dark small color="primary">
+      <v-icon dark>mdi-minus</v-icon>
+    </v-btn>
+   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style>
+
+</style>
